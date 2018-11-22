821cc5c02c7663fc64d8a3d84ecedb6c10447bd3
Rafael Acioly <aciolyr@gmail.com>
2018-11-22 11:00:30 -0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'animal_case',
-    version = '0.1.1',
+    version = '1.2.1',
     author = 'Rafael Acioly',
     author_email = 'aciolyr@gmail.com',
     packages = ['animal_case'],
