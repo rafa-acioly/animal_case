18964ae6de9785bfd09f205097610adfd91c7934
Rafael Acioly <rafael.acioly@luizalabs.com>
2019-08-26 17:09:57 -0300

    adds reflection to avoid if statements and type hints :nail_care:

--- animal_case/__init__.py
+++ animal_case/__init__.py
@@ -1,12 +1,15 @@
+from typing import Dict, List, Union, Iterable
 import re
 
-def _unpack(data):
+from .types import SNAKE_CASE, CAMEL_CASE
+
+def _unpack(data) -> Iterable:
     if isinstance(data, dict):
         return data.items()
     return data
 
 
-def to_snake_case(value):
+def to_snake_case(value: str) -> str:
     """
     Convert camel case string to snake case
     :param value: string
@@ -16,7 +19,7 @@ def to_snake_case(value):
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', first_underscore).lower()
 
 
-def keys_to_snake_case(content):
+def keys_to_snake_case(content: Dict) -> Dict:
     """
     Convert all keys for given dict to snake case
     :param content: dict
@@ -25,7 +28,7 @@ def keys_to_snake_case(content):
     return {to_snake_case(key): value for key, value in _unpack(content)}
 
 
-def to_camel_case(value):
+def to_camel_case(value: str) -> str:
     """
     Convert the given string to camel case
     :param value: string
@@ -35,7 +38,7 @@ def to_camel_case(value):
     return content[0] + ''.join(word.title() for word in content[1:] if not word.isspace())
 
 
-def keys_to_camel_case(content):
+def keys_to_camel_case(content: Dict) -> Dict:
     """
     Convert all keys for given dict to camel case
     :param content: dict
@@ -44,27 +47,25 @@ def keys_to_camel_case(content):
     return {to_camel_case(key): value for key, value in _unpack(content)}
 
 
-def parse_keys(data=None, types='snake'):
+def parse_keys(data: Union[Dict, List] = None, types=SNAKE_CASE) -> Union[Dict, List]:
     """
     Convert all keys for given dict/list to snake case recursively
     the main type are 'snake' and 'camel'
-    :param data: dict
-    :return: dict
-
+    :param data: dict | list
+    :return: dict | list
     """
-    if types not in ('snake', 'camel'):
+    if types not in (SNAKE_CASE, CAMEL_CASE):
         raise ValueError("Invalid parse type, use snake or camel")
+    
+    if not isinstance(data, (list, dict)):
+        raise TypeError("Invalid data type, use list or dict")
+
+    formatter = keys_to_snake_case if types == 'snake' else keys_to_camel_case
+
+    formatted = type(data)()
 
     is_dict = lambda x: type(x) == dict
     is_list = lambda x: type(x) == list
-    formatter = keys_to_snake_case if types == 'snake' else keys_to_camel_case
-
-    if is_dict(data):
-        formatted = {}
-    elif is_list(data):
-        formatted = []
-    else:
-        raise TypeError("Invalid data type, use list or dict")
 
     for key, value in _unpack(formatter(data)):
         if is_dict(value):
